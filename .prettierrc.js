6beba51579894eb092e90c92adfcd1a832bed787
dwiklich <d.wiklich@gmail.com>
2021-07-28 12:20:15 -0300

    Intial config

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // adiciona ponto e virgula no final de onde precisa 
+  trailingComma: 'all', // adiciona virgula no ultimo atributo do objeto
+  singleQuote: true, // aspas simples
+  printWidth: 80, // limita ate 80 da tela na escrita do codigo, depois força a quebrar a linha
+  tabWidth: 2, // aumenta o espaçamento do tab, deixando o espaço de 1 tab valer 2
+}
---
+++ README.old.md
@@ -0,0 +1,6 @@
+# typescript-pomodoro-react
+
+----------------------------------------------
+package.json
+  devDependecies:
+    "eslint": "^7.2.0", o react ja tras o eslint
